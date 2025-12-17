8d6f9cf4916b9f55e6683dab7dfa0c9decdbca3c
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2025-12-17 16:12:56 -0300

    Otimizando o código, removendo a solução bubble sort!

--- Exer_VET_VIII/src/application/Main.java
+++ Exer_VET_VIII/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double sum = 0.0, greater = 0.0, lower = 0.0;
+		double sumWomenHeights = 0.0, greater = 0.0, lower = 0.0;
 		int numberOfWomen = 0;
 
 		System.out.print("How many people will be registered? ");
@@ -28,31 +28,30 @@ public class Main {
 			String gender = sc.next().substring(0, 1);
 
 			people[i] = new Person(height, gender);
-
-			if (people[i].getGender().equalsIgnoreCase("F")) {
-				sum += people[i].getHeight();
-				numberOfWomen++;
-			}
 		}
 		
+		greater = people[0].getHeight();
 		lower = people[0].getHeight();
 		
 		for (int i = 0; i < people.length; i++) {
-			for (int j = 0; j < people.length - 1; j++) {
-				if (people[i].getHeight() > people[j + 1].getHeight() && people[i].getHeight() > greater) {
-					greater = people[i].getHeight();
-				}
-				if (people[i].getHeight() < people[j + 1].getHeight() && people[i].getHeight() < lower) {
-					lower = people[i].getHeight();
-				}
+			if(people[i].getHeight() < lower) {
+				lower = people[i].getHeight();
+			}
+			
+			if(people[i].getHeight() > greater) {
+				greater = people[i].getHeight();
+			}
+			
+			if (people[i].getGender().equalsIgnoreCase("F")) {
+				sumWomenHeights += people[i].getHeight();
+				numberOfWomen++;
 			}
 		}
 		System.out.println("Lower = " + lower);
 		System.out.println("Greater = " + greater);
-		System.out.println("Average Heights = " + String.format("%.2f", sum / numberOfWomen));
+		System.out.println("Average Heights = " + String.format("%.2f", sumWomenHeights / numberOfWomen));
 		System.out.println("Number of Men = " + (quantity - numberOfWomen));
 
 		sc.close();
 	}
-
 }
